949c79950b2a8988f57eaec9b72a79c4886b3fe0
Ontaria Woods <ontaria.woods@gmail.com>
2023-06-30 21:18:42 -0400

    commit tree 842c045da87908eb1191ca5fb379a70566a032b5

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,19 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+    [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  new_array = array.dup
+  new_array << element
+  new_array
 end
 
 def remove_element_from_end_of_array(array)
